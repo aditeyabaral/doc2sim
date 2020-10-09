834b55cf3dc2285dd20c5b8c6c23cdc6dfc70c36
Anderson R. Livramento <andi.devel@gmail.com>
2020-10-09 20:43:47 -0300

    New interface. Allow upload files.
    Ref issue #2.

--- d2v.py
+++ d2v.py
@@ -1,48 +1,13 @@
 import sys
-import numpy as np
-from nltk.tokenize import word_tokenize, sent_tokenize
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from readDocument import getText
 
 from pathlib import Path
 
-import gui
-
-
-def cosine_similarity(A, B):
-    return np.dot(A, B)/(np.linalg.norm(A)*np.linalg.norm(B))
-
-
-def check_similarity(filenames):
-    documents = list(map(getText, filenames))
-    train_text = [
-        TaggedDocument(
-            words=word_tokenize(doc),
-            tags=[str(i)]
-        ) for i, doc in enumerate(documents)
-    ]
-
-    model = Doc2Vec(vector_size=500, window=2, epochs=20, min_count=1)
-    model.build_vocab(train_text)
-    model.train(train_text, total_examples=model.corpus_count, epochs=50)
-
-    test_text = list(map(word_tokenize, documents))
-
-    vecs = list(map(model.infer_vector, test_text))
-
-    total_files = len(filenames)
-    similarity_matrix = np.zeros((total_files, total_files))
-
-    for i in range(total_files):
-        for j in range(total_files):
-            similarity_matrix[i, j] = cosine_similarity(vecs[i], vecs[j])
-    return similarity_matrix
-
+import doc2sim
 
 filenames = sys.argv[1:]
-if '--gui' in filenames:
-    app = gui.create_app(sim_callback=check_similarity, here=Path(__file__).resolve())
-    app.run(port=3000, debug=('--debug' in filenames))
-else:
-    similarity_matrix = check_similarity(filenames)
+
+if filenames:
+    similarity_matrix = doc2sim.check_similarity(filenames)
     print(f"\nSimilarity = \n{similarity_matrix}")
+else:
+    print('\nUsage: d2v.py file1 file2 file3 ...\n')
---
+++ doc2sim/__init__.py
@@ -0,0 +1,34 @@
+import numpy as np
+from nltk.tokenize import word_tokenize, sent_tokenize
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from doc2sim.readDocument import getText
+ 
+
+def cosine_similarity(A, B):
+    return np.dot(A, B)/(np.linalg.norm(A)*np.linalg.norm(B))
+
+
+def check_similarity(filenames):
+    documents = list(map(getText, filenames))
+    train_text = [
+        TaggedDocument(
+            words=word_tokenize(doc),
+            tags=[str(i)]
+        ) for i, doc in enumerate(documents)
+    ]
+
+    model = Doc2Vec(vector_size=500, window=2, epochs=20, min_count=1)
+    model.build_vocab(train_text)
+    model.train(train_text, total_examples=model.corpus_count, epochs=50)
+
+    test_text = list(map(word_tokenize, documents))
+
+    vecs = list(map(model.infer_vector, test_text))
+
+    total_files = len(filenames)
+    similarity_matrix = np.zeros((total_files, total_files))
+
+    for i in range(total_files):
+        for j in range(total_files):
+            similarity_matrix[i, j] = cosine_similarity(vecs[i], vecs[j])
+    return similarity_matrix
